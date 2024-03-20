743217950c92883d0e60b20ed6975bea4706da29
mason.bartholomai1 <mason.bartholomai1@gmail.com>
2024-03-20 13:35:02 +0000

    ESLint configured, rules to turn off annoying blocks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,54 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
   },
-}
+  'extends': [
+    'google',
+    'plugin:react/recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'no-unused-vars': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'off',
+    'max-len': 'off',
+    'require-jsdoc': 'off',
+    'newlines-between-class-members': 'off',
+    'new-cap': 'off',
+    'prefer-rest-params': 'off',
+    'no-var': 'off',
+    'no-invalid-this': 'off',
+    'prefer-const': 'off',
+    'react/display-name': 'off',
+    'prefer-spread': 'off',
+    'guard-for-in': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'warn',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@
  * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
- 
+
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
